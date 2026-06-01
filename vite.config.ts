61d0caf7c54572d30f59ce1cbd327e0c90ca1bc5
FALLAKI500 <125271312+FALLAKI500@users.noreply.github.com>
2026-06-01 20:19:52 +0100

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/SVT-2-me-BAC/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
